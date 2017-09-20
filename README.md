d9e5c2f6b9b9c51ef59424a400f826fe70a7a2c6
Murray Smith <murray@mismith.info>
2017-09-20 16:16:30 -0600

    code cleanup + basic documentation

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # ccd
-
 Custom Change Directory: `cd` with built-in tree navigator
+
+
+## Usage
+- `ccd` to start in the current directory
+- to navigate dirs, use up + down arrow keys
+- to move into a folder (e.g. open it), use right arrow key
+- to move up a level, use left arrow key
+- to select a dir, press enter (or space)
+- to abort, press Ctrl+C (or esc?)
+
+
+## TODO
+- fix empty directories behavior
+- save last-selected dir when moving up a level
+- add key to select currently open directory (rather than one of its subfolders)
--- ccd.sh
+++ ccd.sh
@@ -1,35 +1,27 @@
 #!/bin/bash
 
-# @TODO: `ls -lad */` # list directories
-
 function ccd() {
-  # adapted from: https://bbs.archlinux.org/viewtopic.php?id=105732
-
-  # input: list of menu items
-  # output: item selected
-  # exit codes: 0 - normal, 1 - abort, 2 - no menu items, 3 - too many items
-  # to select item, press enter; to abort press Ctrl+C
+  # some concepts adapted from: https://bbs.archlinux.org/viewtopic.php?id=105732
 
   DIRS=(*/)
 
   TOTAL_NUMBER=${#DIRS[@]} # total number of items
-
   DIRS+=(..) # store hidden "up-a-level" dir
 
-  #[[ $TOTAL_NUMBER -lt 1 ]] && exit 2 # no menu items, at least 1 required
+  # TODO [[ $TOTAL_NUMBER -lt 1 ]] && exit 1 # no subfolders
 
-  [[ $TOTAL_NUMBER -gt $(( `tput lines` - 1 )) ]] && exit 3 # more items than rows
+  # TODO: [[ $TOTAL_NUMBER -gt $(( `tput lines` - 1 )) ]] && exit 3 # more items than rows
 
   # keys
+  ESCAPE="`echo -e '\x1b'`" # escape-character
   ARROW_UP="`echo -e '\x1b[A'`" # arrow up
   ARROW_DOWN="`echo -e '\x1b[B'`" # arrow down
   ARROW_RIGHT="`echo -e '\x1b[C'`" # arrow right
   ARROW_LEFT="`echo -e '\x1b[D'`" # arrow left
-  ESCAPE="`echo -e '\x1b'`"   # escape
-  NEWLINE="`echo -e '\n'`"   # newline
+  NEWLINE="`echo -e '\n'`" # newline
 
+  # clear current output
   clearlist() {
-    # clear current output
     for j in `seq 1 $(( $TOTAL_NUMBER + 1 ))`
     do
       tput cuu1 # move up a line
@@ -40,65 +32,63 @@ function ccd() {
 
   { # capture stdout to stderr
 
-  #tput civis # hide cursor
-  CURRENT_POSITION=1 # current position # @TODO: set to previously selected directory if moved up a level
-  END=false
-  RECURSE=false
+    CURRENT_POSITION=1 # current position
+    END=false
+    RECURSE=false
 
-  pwd
+    pwd
 
-  while ! $END
-  do
-    for i in `seq 1 $TOTAL_NUMBER`
+    while ! $END
     do
-      echo -n "  "
-      [[ $CURRENT_POSITION == $i ]] && tput rev # reverse styling if currently selected item
-      echo -e "${DIRS[$i - 1]}"
-      tput sgr0 # reset all styles
+      for i in `seq 1 $TOTAL_NUMBER`
+      do
+        echo -n "  "
+        [[ $CURRENT_POSITION == $i ]] && tput rev # reverse styling if currently selected item
+        echo -e "${DIRS[$i - 1]}"
+        tput sgr0 # reset all styles
+      done
+
+      read -sn 1 KEY
+      [[ "$KEY" == "$ESCAPE" ]] &&
+      {
+        read -sn 2 ESCAPED_KEY
+        KEY="$KEY$ESCAPED_KEY"
+      }
+
+      case "$KEY" in
+        "$ARROW_UP")
+          CURRENT_POSITION=$(( CURRENT_POSITION - 1 )) # move selector up
+          [[ $CURRENT_POSITION == 0 ]] && CURRENT_POSITION=$TOTAL_NUMBER # loop
+          ;;
+        "$ARROW_DOWN")
+          CURRENT_POSITION=$(( CURRENT_POSITION + 1 )) # move selector down
+          [[ $CURRENT_POSITION == $(( TOTAL_NUMBER + 1 )) ]] && CURRENT_POSITION=1 # loop
+          ;;
+        "$ARROW_RIGHT")
+          # move into this folder, then load its subdirectories in place
+          clearlist
+          SELECTED=true
+          END=true
+          RECURSE=true
+          ;;
+        "$ARROW_LEFT")
+          # cd up a level and load its subdirectories in place
+          CURRENT_POSITION=$(( TOTAL_NUMBER + 1 )) # use hidden dir
+          clearlist
+          SELECTED=true
+          END=true
+          RECURSE=true
+          ;;
+        "$NEWLINE")
+          SELECTED=true
+          END=true
+          ;;
+      esac
+
+      tput cuu $TOTAL_NUMBER # move cursor to top for next time
     done
 
-    read -sn 1 KEY
-    [[ "$KEY" == "$ESCAPE" ]] &&
-    {
-      read -sn 2 ESCAPED_KEY
-      KEY="$KEY$ESCAPED_KEY"
-    }
-
-    case "$KEY" in
-      "$ARROW_UP")
-        CURRENT_POSITION=$(( CURRENT_POSITION - 1 )) # move selector up
-        [[ $CURRENT_POSITION == 0 ]] && CURRENT_POSITION=$TOTAL_NUMBER # loop
-        ;;
-      "$ARROW_DOWN")
-        CURRENT_POSITION=$(( CURRENT_POSITION + 1 )) # move selector down
-        [[ $CURRENT_POSITION == $(( TOTAL_NUMBER + 1 )) ]] && CURRENT_POSITION=1 # loop
-        ;;
-      "$ARROW_RIGHT")
-        # move into this folder, then load its subdirectories in place
-        clearlist
-        SELECTED=true
-        END=true
-        RECURSE=true
-        ;;
-      "$ARROW_LEFT")
-        # cd up a level and load its subdirectories in place
-        CURRENT_POSITION=$(( TOTAL_NUMBER + 1 )) # use hidden dir
-        clearlist
-        SELECTED=true
-        END=true
-        RECURSE=true
-        ;;
-      "$NEWLINE")
-        SELECTED=true
-        END=true
-        ;;
-    esac
-
-    tput cuu $TOTAL_NUMBER # move cursor to top for next time
-  done
-
-  $RECURSE && tput cuu 1 || tput cud $TOTAL_NUMBER # move cursor back to bottom if done, or very top if recursing
-  #tput cnorm # unhide cursor
+    $RECURSE && tput cuu 1 || tput cud $TOTAL_NUMBER # move cursor back to bottom if done, or very top if recursing
 
   } >&2 # end capture
 
